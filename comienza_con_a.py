fc900dd6fd255715ca67c32ccc12df61af3da90c
KarenDC5 <karen.duran@iteso.mx>
2025-02-17 17:18:09 -0600

    primer

--- comienza_con_a.py
+++ comienza_con_a.py
@@ -1,15 +1,25 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Determinar si una palabra comienza con la letra "a"
+Karen Durán
+757136
+Ingeniería CiviL
+Algoritmos y Programación 
+ESI232B2
+14/02/25
+10 minutos 
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+palabra = input("Escribe una palabra: ")
 
 # Proceso
-
+if palabra [0] == 'a':
+    comienza = True 
+else: 
+    comienza = False  
 
 # Salidas
-print(salida)
+if comienza: 
+    print(palabra, "comienza con 'A'")
+else: 
+    print(palabra, "no comienza con 'A'")
