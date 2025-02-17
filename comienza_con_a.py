980d75854cc4ad53f7518d31bc26e535afce11e7
KarenDC5 <karen.duran@iteso.mx>
2025-02-17 17:30:17 -0600

    segundo

--- comienza_con_a.py
+++ comienza_con_a.py
@@ -6,20 +6,20 @@ Ingeniería CiviL
 Algoritmos y Programación 
 ESI232B2
 14/02/25
-10 minutos 
+15 minutos 
 """
 
 # Entradas
-palabra = input("Escribe una palabra: ")
+palabra = input("Escribe una palabra:")
 
 # Proceso
-if palabra [0] == 'a':
+if palabra [0] in ['a', 'á', 'A', 'Á']:
     comienza = True 
 else: 
     comienza = False  
 
 # Salidas
 if comienza: 
-    print(palabra, "comienza con 'A'")
+    print(palabra, "comienza con 'A'") 
 else: 
     print(palabra, "no comienza con 'A'")
